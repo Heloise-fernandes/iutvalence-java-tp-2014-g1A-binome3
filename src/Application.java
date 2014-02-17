535644e11765b5c8315471d9ca3979f239b975d4
Fernandes Heloise <heloise.fernandesdealmeida@iut-valence.fr>
2014-02-17 17:58:49 +0100

    premier jet

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+
+public class Application {
+
+	public static void main(String[] args) 
+	{
+		Mastermind Newgame = new Mastermind();
+		
+
+	}
+
+}
---
+++ src/Mastermind.java
@@ -0,0 +1,39 @@
+
+public class Mastermind 
+{
+	private final int Vert = 1 ;
+	private final int Rouge = 2 ;
+	
+	private final int C_Rouge = 11;
+	
+	private int n ;
+	
+	
+	
+	public void Jouer ()
+	{
+		System.out.println("***Menu***");
+		System.out.println("Choisissez le nombre de joueur (1 ou 2) :");
+		//recuperation de la reponse
+		//......
+		//Appel de la méthode correspondante au condition de jeu
+		//Fin du menu
+	}
+
+	public class Pion
+	{
+		private int couleur;
+		private int position;
+		
+		public Pion (int c,int p)
+		{
+			this.couleur = c;
+			this.position = p;
+		}
+	}
+	
+	public class Code
+	{
+		
+	}
+}
