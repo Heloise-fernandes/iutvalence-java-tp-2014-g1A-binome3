9d72a523c1caccab2065f94a95a95f3b00faf9f0
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:24:38 +0100

    TODO Javadoc (Code.java)

--- src/Code.java
+++ src/Code.java
@@ -1,26 +1,30 @@
+/* TODO Javadoc */
+/* TODO Package. */
 
 public class Code 
 {
 	private final int longueur;
-	Pion[] mesPions = null;
-	
+    /* TODO Final ? */
+	Pion[] mesPions;
+
+    /* TODO Par convention en Java les noms de parametres n'ont pas de '_'. */
 	public Code(int niv_longueur)
 	{
 		this.longueur = niv_longueur;
 		this.mesPions = new Pion[niv_longueur];
 	}
-	
-	public void CodeSecretAuto(Code monCodeSecret)
+
+    /* TODO Par convention en Java les noms de méthodes n'ont pas de majuscule. */
+    public void CodeSecretAuto(Code monCodeSecret)
 	{
 		for (int i=0; i<=monCodeSecret.longueur; i++)
 		{
 			monCodeSecret.mesPions[i] = random(Couleur);
 			//A chercher comment utiliser la fonction random
 		}
-		
-		
 	}
-	public void ajouterPion (Pion choixPion)
+
+	public void ajouterPion(Pion choixPion)
 	{
 		mesPions[choixPion.getPosition()] = choixPion;
 	}
