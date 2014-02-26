859e9bda3903bbb0df7ba17773cc4c52b14477fc
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:25:23 +0100

    TODO Javadoc (Couleur.java)

--- src/Couleur.java
+++ src/Couleur.java
@@ -1,6 +1,9 @@
+/* TODO Javadoc */
+/* TODO Package. */
 
 public enum Couleur 
 {
+    /* TODO Par convention, les enum sont des constantes, donc le nom de leurs éléments est en majuscules. */
 	Vert,
 	Rouge,
 	Bleu,
