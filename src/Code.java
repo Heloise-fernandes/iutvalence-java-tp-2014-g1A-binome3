8fe09edad1b6bb0052bd2e87c13db60e9ec5c66c
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 11:35:54 +0100

    TODO Javadoc (Code.java)

--- src/Code.java
+++ src/Code.java
@@ -1,10 +1,11 @@
-import java.util.Random;
-
 /* TODO Javadoc */
 /* TODO Package. */
 
-public class Code 
+import java.util.Random;
+
+public class Code
 {
+    /* TODO Est-ce utile de retenir les deux informations ? Ne peut-on pas inférer la première par la seconde ? */
 	private final int longueur;
     /* TODO Final ? */
 	Pion[] mesPions;
@@ -15,28 +16,37 @@ public class Code
 		this.mesPions = new Pion[nivLongueur];
 	}
 
+    /* TODO Est-ce que ce n'est pas une autre forme de constructeur ? */
     public static void codeSecretAuto(Code monCodeSecret)
 	{
 		for (int i=0; i<=monCodeSecret.longueur; i++)
 		{
+            /* TODO On préférera SecureRandom à Random. */
 			int pick = new Random().nextInt(Couleur.values().length);
 		    monCodeSecret.mesPions[i].changerCouleur(Couleur.values()[pick]);
 		}
 	}
 
+    /* TODO Vous n'utilisez jamais cette méthode. */
 	public void ajouterPion(Pion choixPion)
 	{
 		mesPions[choixPion.getPosition()] = choixPion;
 	}
-	
+
+    /* TODO Pourquoi static ? */
+    /* TODO Pourquoi d'autres paramètres ? */
+    /* TODO Etes-vous sur que ça marche ? */
+    /* TODO Algo à retravailler. */
 	public static Code testMatch(Code bonCode, Code testCode)
 	{
 		int k=0;
 		int l=0;
 		boolean conf=false;
+        /* TODO Pourquoi forcer la taille à 5 ? */
 		Code codeCorrection = new Code(5);
 		while (k<5)
 		{
+            /* TODO Vous ne devriez pas comparer des objets avec '==' mais avec la méthode equals. */
 			if(bonCode.mesPions[k]==testCode.mesPions[k])
 			{
 				codeCorrection.mesPions[k].changerCouleur(Couleur.VERT);
@@ -46,13 +56,15 @@ public class Code
 			{
 				while (l<5)
 				{
-					if(testCode.mesPions[k]==bonCode.mesPions[l])
+                    /* TODO Vous ne devriez pas comparer des objets avec '==' mais avec la méthode equals. */
+                    if(testCode.mesPions[k]==bonCode.mesPions[l])
 					{
 						codeCorrection.mesPions[k].changerCouleur(Couleur.ROUGE);
 						conf=true;
 					}
 					l++;
 				}
+                /* TODO Pourquoi pas simplement "!conf" ? */
 				if(conf==false)
 				{
 					codeCorrection.mesPions[k].changerCouleur(Couleur.NOIR);
