d06cea7b219433f8918e4983fb7e8eb434d01444
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 11:38:02 +0100

    Narrow scope of variables.

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -6,24 +6,22 @@ public class Mastermind
 
 	public boolean niveau()
 	{
-		int j = 0;
-		int i = 0;
-		int k;
-		int l = 0;
-		int x;
-		Code leCodeSecret = new Code(5);
+        Code leCodeSecret = new Code(5);
 		Code.codeSecretAuto(leCodeSecret);
 		Code codeValidation = new Code(5);
 		Scanner sc = new Scanner(System.in);
-		while (j<15)
+        int j = 0;
+        int i = 0;
+        int l = 0;
+        while (j<15)
 		{
 			Code monNouveauCode= new Code(5);
 			while (i<5)
 			{
 				System.out.println("0=Vert\n 1=Rouge\n 2=Bleu\n 3=Orange\n 4=Jaune\n 5=Blanc\n 6=Noir\n 7=Violet\n 8=Rose\n 9=Marron\n");
 				System.out.println("Choisir une couleur:");
-				x = sc.nextInt();
-				if(x>9)
+                int x = sc.nextInt();
+                if(x>9)
 				{
 					System.out.println("Vous etes un boulet, retournez jouer a Tetris.");
 					i--;
@@ -38,8 +36,8 @@ public class Mastermind
 			
 			//On regarde la validiter du code et on creer le code correction
 			codeValidation=Code.testMatch(leCodeSecret,monNouveauCode);
-			k=0;
-			System.out.println(codeValidation);
+            int k = 0;
+            System.out.println(codeValidation);
 			while(k<5)
 			{
 				if(codeValidation.mesPions[k].getCouleur()!=Couleur.VERT)
