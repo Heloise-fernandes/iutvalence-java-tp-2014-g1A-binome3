dbc7b90a06b8cb9b90f41b28d2a4e556bde28ae2
Anthony Gelibert <anthony.gelibert@me.com>
2014-05-25 23:37:00 +0200

    Create a real Application.java with a param to select GUI.

--- src/Application.java
+++ src/Application.java
@@ -1,20 +1,25 @@
+import IHM.AffichageFenetre;
+
 import javax.swing.SwingUtilities;
 
 /**
  * Controleur de l'application
  *
  * @author Desmet/Fernandes
- *
+ * @version 3.0.0
  */
 public class Application {
     /** main du programme */
-	public static void main(String[] args) 
-	{ 
-			//SwingUtilities.invokeLater(new AffichageFenetre());
-			/*Vue vue=new Vue();
-			int niveauChoisi=vue.choixNiveau();
-			String nom=vue.choisirJoueur();
-			Mastermind newGame = new Mastermind(niveauChoisi,vue,nom);
-			vue.gagner(newGame.jouer(vue));*/
-	}
+    public static void main(String[] args) {
+        if ((args.length == 1) && "--gui".equals(args[0])) { /* NON-NLS */
+            SwingUtilities.invokeLater(new AffichageFenetre());
+        }
+        else {
+            Vue vue = new Vue();
+            int niveauChoisi = vue.choixNiveau();
+            String nom = vue.choisirJoueur();
+            Mastermind newGame = new Mastermind(niveauChoisi, vue, nom);
+            vue.gagner(newGame.jouer(vue));
+        }
+    }
 }
