96ac60cf8cad67e90f78bba841c99b83ec2dd2ce
Anthony Gelibert <anthony.gelibert@me.com>
2014-05-25 23:27:19 +0200

    Custom JPanel not required as type in AffichageFenetre.

--- src/IHM/AffichageFenetre.java
+++ src/IHM/AffichageFenetre.java
@@ -1,29 +1,25 @@
 package IHM;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JSplitPane;
-import javax.swing.JTable;
+import java.awt.Color;
 public class AffichageFenetre implements Runnable
 {
-	
-	PanneauDeCorrection panneauGauche;
-	PanneauSaisieDeCode panneauCentre;
-	PanneauDeValidation panneauDroit;
-	@Override
-	public void run()
-	{
-		
-		JFrame fenetre = new JFrame();
-		this.panneauGauche= new PanneauDeCorrection();
-		this.panneauCentre= new PanneauSaisieDeCode();
-		this.panneauDroit= new PanneauDeValidation();
-		
+    private JPanel panneauGauche;
+    private JPanel panneauCentre;
+    private JPanel panneauDroit;
 
-		fenetre.setTitle("Mastermind");
+    @Override
+    public void run() {
+
+        JFrame fenetre = new JFrame();
+        this.panneauGauche = new PanneauDeCorrection();
+        this.panneauCentre = new PanneauSaisieDeCode();
+        this.panneauDroit = new PanneauDeValidation();
+
+
+        fenetre.setTitle("Mastermind");
 		fenetre.setSize(500,500);
 		
 		JSplitPane splitPanerGauche= new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
