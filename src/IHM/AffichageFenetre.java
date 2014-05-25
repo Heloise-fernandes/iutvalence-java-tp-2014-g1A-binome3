b591d18919962597fa8c0bb3996ae31c00012881
Anthony Gelibert <anthony.gelibert@me.com>
2014-05-25 23:35:18 +0200

    Simplify AffichageFenetre.java.

--- src/IHM/AffichageFenetre.java
+++ src/IHM/AffichageFenetre.java
@@ -1,50 +1,23 @@
 package IHM;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSplitPane;
-import java.awt.Color;
-public class AffichageFenetre implements Runnable
-{
-    private JPanel panneauGauche;
-    private JPanel panneauCentre;
-    private JPanel panneauDroit;
+import java.awt.BorderLayout;
+import java.awt.Container;
 
+public class AffichageFenetre implements Runnable {
     @Override
     public void run() {
+        JFrame fenetre = new JFrame("Mastermind");
+        fenetre.setSize(500, 500);
+        fenetre.setResizable(false);
 
-        JFrame fenetre = new JFrame();
-        this.panneauGauche = new PanneauDeCorrection();
-        this.panneauCentre = new PanneauSaisieDeCode();
-        this.panneauDroit = new PanneauDeValidation();
+        Container global = fenetre.getContentPane();
+        global.setLayout(new BorderLayout());
+        global.add(new PanneauDeCorrection(), BorderLayout.WEST);
+        global.add(new PanneauDeValidation(), BorderLayout.EAST);
+        global.add(new PanneauSaisieDeCode(), BorderLayout.CENTER);
 
-
-        fenetre.setTitle("Mastermind");
-		fenetre.setSize(500,500);
-		
-		JSplitPane splitPanerGauche= new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-		
-		splitPanerGauche.add(panneauGauche);
-		splitPanerGauche.add(panneauCentre);
-		splitPanerGauche.setEnabled(false);
-		splitPanerGauche.setBorder(null);
-		splitPanerGauche.setDividerSize(1);
-		
-		
-		JSplitPane splitPanerDroit= new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-		
-		
-		splitPanerDroit.add(splitPanerGauche);
-		splitPanerDroit.add(panneauDroit);
-		splitPanerDroit.setEnabled(false);
-		splitPanerDroit.setBorder(null);
-		splitPanerDroit.setDividerSize(1);
-		
-		splitPanerDroit.setOpaque(true);	
-		splitPanerDroit.setBackground(new Color(100,200,100));		
-		fenetre.add(splitPanerDroit);
-
-		fenetre.setVisible(true);
-		fenetre.repaint();
-	}
+        fenetre.setVisible(true);
+        fenetre.repaint();
+    }
 }
