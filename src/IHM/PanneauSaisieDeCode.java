63cb32c87d088db0251f2febd669c7199fe75cb8
Anthony Gelibert <anthony.gelibert@me.com>
2014-05-25 23:40:24 +0200

    Indentation (PanneauSaisieDeCode.java)

--- src/IHM/PanneauSaisieDeCode.java
+++ src/IHM/PanneauSaisieDeCode.java
@@ -1,35 +1,26 @@
 package IHM;
-import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 
-public class PanneauSaisieDeCode extends JPanel
-{
-	private JButton[][] pionSaisie;
-	
-	public PanneauSaisieDeCode()
-	{
-		this.setLayout(new GridLayout(10, 5));
-		this.pionSaisie= new BoutonChangeCouleur[10][5];
-		
-		for (int indice=0; indice<10; indice++)
-		{
-			for(int index=0; index<5; index++)
-			{
-				pionSaisie[indice][index]=new BoutonChangeCouleur();
-				this.add(this.pionSaisie[indice][index]);
-				pionSaisie[indice][index].ActionListener()
-				if (indice>0)
-				{
-					this.pionSaisie[indice][index].setEnabled(false);
-				}
-				
-			}
-			
-			
-			
-		}
-	}
+public class PanneauSaisieDeCode extends JPanel {
+    private JButton[][] pionSaisie;
+
+    public PanneauSaisieDeCode() {
+        this.setLayout(new GridLayout(10, 5));
+        this.pionSaisie = new BoutonChangeCouleur[10][5];
+
+        for (int indice = 0; indice < 10; indice++) {
+            for (int index = 0; index < 5; index++) {
+                pionSaisie[indice][index] = new BoutonChangeCouleur();
+                this.add(this.pionSaisie[indice][index]);
+                //pionSaisie[indice][index].ActionListener()
+                if (indice > 0) {
+                    this.pionSaisie[indice][index].setEnabled(false);
+                }
+            }
+        }
+    }
 }
