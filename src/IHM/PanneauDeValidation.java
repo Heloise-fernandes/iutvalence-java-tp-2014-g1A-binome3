7b86c15f61538ec5067ca27734de3d99e9c55151
Anthony Gelibert <anthony.gelibert@me.com>
2014-05-25 23:53:21 +0200

    Update all JPanel.

--- src/IHM/PanneauDeValidation.java
+++ src/IHM/PanneauDeValidation.java
@@ -1,25 +1,31 @@
 package IHM;
-import java.awt.GridLayout;
-
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
 
-
+/**
+ * TODO.
+ *
+ * @author TODO
+ * @version TODO
+ */
 public class PanneauDeValidation extends JPanel
 {
+    /** Serial version UID. */
+    private static final long serialVersionUID = 1L;
 	private JButton[] boutonValidation;
 	
 	public PanneauDeValidation()
 	{
-		this.setLayout(new GridLayout(10, 1));
-		this.boutonValidation= new JButton[10];
+		setLayout(new GridLayout(10, 1));
+		boutonValidation= new JButton[10];
 		for (int indice=0; indice<10; indice++)
 		{
 			boutonValidation[indice]=new JButton("OK");
-			this.add(this.boutonValidation[indice]);
+			add(this.boutonValidation[indice]);
 			if (indice>0)
 			{
-				this.boutonValidation[indice].setEnabled(false);
+				boutonValidation[indice].setEnabled(false);
 			}
 			
 			
--- src/IHM/PanneauSaisieDeCode.java
+++ src/IHM/PanneauSaisieDeCode.java
@@ -4,21 +4,27 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 
-
+/**
+ * TODO.
+ *
+ * @author TODO
+ * @version TODO
+ */
 public class PanneauSaisieDeCode extends JPanel {
-    private JButton[][] pionSaisie;
+    /** Serial version UID. */
+    private static final long serialVersionUID = 1L;
+    private final JButton[][] pionSaisie;
 
     public PanneauSaisieDeCode() {
-        this.setLayout(new GridLayout(10, 5));
-        this.pionSaisie = new BoutonChangeCouleur[10][5];
+        setLayout(new GridLayout(10, 5));
+        pionSaisie = new BoutonChangeCouleur[10][5];
 
         for (int indice = 0; indice < 10; indice++) {
             for (int index = 0; index < 5; index++) {
                 pionSaisie[indice][index] = new BoutonChangeCouleur();
-                this.add(this.pionSaisie[indice][index]);
-                //pionSaisie[indice][index].ActionListener()
+                add(pionSaisie[indice][index]);
                 if (indice > 0) {
-                    this.pionSaisie[indice][index].setEnabled(false);
+                    pionSaisie[indice][index].setEnabled(false);
                 }
             }
         }
