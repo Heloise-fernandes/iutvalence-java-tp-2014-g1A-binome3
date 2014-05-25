d679690a29a01065b610c752f24c192b71bdeb13
Anthony Gelibert <anthony.gelibert@me.com>
2014-05-25 23:39:30 +0200

    Correct BoutonChangeCouleur.

--- src/IHM/BoutonChangeCouleur.java
+++ src/IHM/BoutonChangeCouleur.java
@@ -1,19 +1,31 @@
 package IHM;
-import java.awt.Color;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-@SuppressWarnings("serial")
-public class BoutonChangeCouleur extends JButton implements ActionListener
-{
-	public BoutonChangeCouleur()
-	{
-		this.addActionListener(this);
-	}
-	
-	public void changerCouleur(ActionListener e)
-	{
-		this.setBackground(Color.BLUE);
-	}
+/**
+ * TODO.
+ *
+ * @author TODO
+ * @version TODO
+ */
+public class BoutonChangeCouleur extends JButton implements ActionListener {
+    /** Serial version UID. */
+    private static final long serialVersionUID = 1L;
+
+    public BoutonChangeCouleur() {
+        this.addActionListener(this);
+    }
+
+    private void changerCouleur() {
+        // XXX À terminer.
+        this.setBackground(Color.BLUE);
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        changerCouleur();
+    }
 }
